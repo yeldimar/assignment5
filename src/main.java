7466e65843e0e545421469aa0c485eacc507df80
Yahya Eldimardash <yeldimar@asu.edu>
2025-02-19 06:11:44 +0200

    created main.java

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/*
+ * File: Main.java
+ * Description: Entry point for the Airport Simulation project (Submission A)
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Airport Simulation!");
+    }
+}
